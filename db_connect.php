f9dc0ba1121c2ecb266428f6939168451782d7bf
Zain Hasan <64851463+zainnada@users.noreply.github.com>
2025-06-27 19:49:36 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,4 @@
 <?php
-//include 'db.php'; // الاتصال بقاعدة البيانات
 
 $servername = "localhost";
 $username = "root";
@@ -8,7 +7,7 @@ $dbname = "zain_library_db";
 
 // error of the socket
 $conn = new mysqli($servername, $username, $password, $dbname, 3306, '/var/run/mysqld/mysqld.sock'); // solution 1: assign the true socket file
-//$conn = new mysqli($servername, $username, $password, $dbname); // solution 2: make mysqli using TCP/IP instead of using the socket
+//$conn = new mysqli("127.0.0.1", $username, $password, $dbname); // solution 2: make mysqli using TCP/IP "127.0.0.1" instead of using the socket
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
