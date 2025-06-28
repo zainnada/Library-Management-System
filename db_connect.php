0aa419febb57765ca0926e927ab5e0e0c6292883
Zain Hasan <64851463+zainnada@users.noreply.github.com>
2025-06-28 16:01:07 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -6,6 +6,7 @@ $password = "";
 $dbname = "zain_library_db";
 
 // error of the socket
+//$conn = new mysqli($servername, $username, $password, $dbname); // maybe you will not face any error with this..
 $conn = new mysqli($servername, $username, $password, $dbname, 3306, '/var/run/mysqld/mysqld.sock'); // solution 1: assign the true socket file
 //$conn = new mysqli("127.0.0.1", $username, $password, $dbname); // solution 2: make mysqli using TCP/IP "127.0.0.1" instead of using the socket
 if ($conn->connect_error) {
